accef2560ec6074e57cd4470362e25903befd017
SolarSpectre <josephcaza32@gmail.com>
2024-06-21 17:06:08 -0500

    add createGitDirectory function

--- app/main.js
+++ app/main.js
@@ -1,7 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
 //constants
 const command = process.argv[2];
 const argument = process.argv[4];
+function createGitDirectory() {
+   fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
+   fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
+   fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
 
+   fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
+   console.log("Initialized git directory");
+ }
 function checkCommand(command){
     switch (command) {
     case "init":
