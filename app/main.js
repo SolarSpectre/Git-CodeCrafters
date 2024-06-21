284b4158922b3b6159a4b389ed6f90bef422779c
SolarSpectre <josephcaza32@gmail.com>
2024-06-21 17:08:59 -0500

    add function to read a blob object

--- app/main.js
+++ app/main.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib")
 
 //constants
 const command = process.argv[2];
 const argument = process.argv[4];
+
+//Read Blob Object
+async function catFile(hash){
+    const contentFile = await fs.readFileSync(path.join(process.cwd(), ".git", "objects", hash.substring(0, 2), hash.substring(2)));
+    const dataUnzipped = zlib.inflateSync(contentFile);
+    const dataString = dataUnzipped.toString('utf8');
+    const content = dataString.substring(dataString.indexOf('\0')+1);
+    process.stdout.write(content);
+}
 function createGitDirectory() {
    fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
    fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
@@ -17,9 +27,11 @@ function checkCommand(command){
     case "init":
         createGitDirectory();
         break;
+    case "cat-file":
+        catFile(argument);
+        break;
     default:
         throw new Error(`Unknown command ${command}`);
     }
 }
-
  checkCommand(command);
